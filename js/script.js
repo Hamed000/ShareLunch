f61e296d5219aded7d26cba9b1f70a4fe8122ba6
Hamed OROU ZIME <hamedorouzime13@gmail.com>
2018-02-10 09:48:17 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// code jQuery with stickiy plugin
+
+var zu8exfi145agie;(function(d, t) {
+	var s = d.createElement(t), options = {
+	'userName':'sharelunch',
+	'formHash':'zu8exfi145agie',
+	'autoResize':true,
+	'height':'306',
+	'async':true,
+	'host':'wufoo.com',
+	'header':'show',
+	'ssl':true};
+	s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
+	s.onload = s.onreadystatechange = function() {
+	var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+	try { zu8exfi145agie = new WufooForm();zu8exfi145agie.initialize(options);zu8exfi145agie.display(); } catch (e) {}};
+	var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
+	})(document, 'script');
+
+
